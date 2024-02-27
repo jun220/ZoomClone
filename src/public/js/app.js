e28ba639baeceadd8048f8f26d25ee2af0dd50a2
jun220 <jun11011100@gmail.com>
2024-02-27 17:40:43 +0900

    feat: 채팅 기능 추가

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,10 +1,14 @@
-console.log("Rikka's the best")
-var result = confirm("릿카는 가장 귀엽습니까?");
-if(result){
-    alert("당연하죠! 역시 릿카..");
-}else{
-    alert("뭘 모르시네..");
-}
+console.log("Rikka's the best");
+
+const messageList = document.querySelector("ul");
+const nickForm = document.querySelector("#nick");
+const messageForm = document.querySelector("#message");
+// var result = confirm("릿카는 가장 귀엽습니까?");
+// if(result){
+//     alert("당연하죠! 역시 릿카..");
+// }else{
+//     alert("뭘 모르시네..");
+// }
 
 const socket = new WebSocket(`ws://${window.location.host}`)
 socket.addEventListener("open", ()=>{
@@ -12,13 +16,42 @@ socket.addEventListener("open", ()=>{
 })
 
 socket.addEventListener("message", (message) => {
-    console.log("Just got this: ", message, " from the server")
+    const li = document.createElement("li");
+    li.innerText = message.data;
+    messageList.append(li);
+    console.log("New message : \"", message.data, "\" from the server")
 })
 
 socket.addEventListener("close", () => {
     console.log("Disconnected from server")
 })
 
+// setTimeout(() => {
+//     console.log("time's up!");
+//     socket.send("hello from the browser!");
+// }, 10000)
+
+function makeMessage(type, payload){
+    const msg = {type, payload};
+    return JSON.stringify(msg);
+}
+
+function handleSubmit(event){
+    event.preventDefault();
+    //input 태그
+    const input = messageForm.querySelector("input");
+    socket.send(makeMessage("message", input.value))
+    input.value = "";
+}
+
+function handleNickSubmit(event){
+    event.preventDefault();
+    //input 태그
+    const input = nickForm.querySelector("input");
+    socket.send(makeMessage("nickname", input.value))
+}
+messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
 // function fn(event){
 //     console.log('submit')
 // }
--- src/server.js
+++ src/server.js
@@ -14,16 +14,34 @@ app.get('/', (req, res)=>{
 
 const handleListen = () => console.log(`Listening on http,ws. On port 3000`);
 
+
+
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-function handleConnection(socket) {
-    socket.send('hello')
+const sockets = [];
+
+function parseMessage(message){
+
+}
+
+wss.on('connection', (socket) => {
+    sockets.push(socket);
+    socket['nickname'] = 'anonymous'
+    console.log('connected from client')
     socket.on("close", () => {
         console.log("disconnected from client")
     })
-}
-
-wss.on('connection', handleConnection)
+    socket.on("message", (message) => {
+        const data = JSON.parse(message);
+        switch(data.type){
+            case 'message':
+                sockets.forEach(soc => soc.send(`${socket.nickname} : ${data.payload}`));
+            case 'nickname':
+                socket['nickname'] = data.payload;
+                console.log(`set nickname : ${data.payload}`);
+        }
+    });
+})
 
 server.listen(3000, handleListen);
